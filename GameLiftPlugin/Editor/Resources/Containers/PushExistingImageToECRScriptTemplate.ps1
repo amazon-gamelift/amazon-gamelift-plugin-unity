3da936aee7b72f0e97cf92e94fd6cf67a9ce2181
Yifan Zhang <yifanzyf@amazon.com>
2025-05-29 12:01:45 -0700

    Resolve conflicts

--- GameLiftPlugin/Editor/Resources/Containers/PushExistingImageToECRScriptTemplate.ps1
+++ GameLiftPlugin/Editor/Resources/Containers/PushExistingImageToECRScriptTemplate.ps1
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:GameLiftPlugin/Editor/Resources/Containers/PushExistingImageToECRScriptTemplate.ps1
 echo "Running aws ecr get-login-password --region {{REGION}} --profile {{PROFILE_NAME}} | docker login --username AWS --password-stdin {{ECR_REGISTRY_URL}}"
 aws ecr get-login-password --region {{REGION}} --profile {{PROFILE_NAME}} | docker login --username AWS --password-stdin {{ECR_REGISTRY_URL}}
 if (!$LastExitCode -eq 0) {
@@ -17,30 +16,7 @@ if ($LastExitCode -eq 0)
   {
     echo "Docker image successfully pushed to Amazon ECR."
   }
-else 
+else
   {
     echo "Docker push to Amazon ECR has failed."
-=======
-echo "Running aws ecr get-login-password --region {{REGION}} --profile {{PROFILE_NAME}} | docker login --username AWS --password-stdin {{ECR_REGISTRY_URL}}"
-aws ecr get-login-password --region {{REGION}} --profile {{PROFILE_NAME}} | docker login --username AWS --password-stdin {{ECR_REGISTRY_URL}}
-if (!$LastExitCode -eq 0) {
-  echo "Docker login has failed."
-  exit $LastExitCode
-}
-echo "Running docker tag {{IMAGE_ID}} {{ECR_REPO_URI}}:{{IMAGE_TAG}}"
-docker tag {{IMAGE_ID}} {{ECR_REPO_URI}}:{{IMAGE_TAG}}
-if (!$LastExitCode -eq 0) {
-  echo "Docker tag has failed."
-  exit $LastExitCode
-}
-echo "Running docker push {{ECR_REPO_URI}}:{{IMAGE_TAG}}"
-docker push {{ECR_REPO_URI}}:{{IMAGE_TAG}}
-if ($LastExitCode -eq 0)
-  {
-    echo "Docker image successfully pushed to Amazon ECR."
-  }
-else 
-  {
-    echo "Docker push to Amazon ECR has failed."
->>>>>>> main:Editor/Resources/Containers/PushExistingImageToECRScriptTemplate.ps1
   }
